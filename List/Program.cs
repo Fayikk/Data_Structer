d22e62681ffd207c698bcec98762b1221ac0a8d0
Fayik <veznedaroglufayik2@gmail.com>
2022-07-11 00:46:31 +0300

    as

---
+++ List/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //tanımlama
+            //new referans tip oluştururken kullanılır.
+            //var anahtar sözcüğü ile sayıalrın tipine çalışma zamanında karar verebilirsiniz.
+            //List<int> sayilar = new List<int>(); 
+
+            string program = "Python";
+            string[] seri = new string[] { "product", "company", "apple","banana" };
+            var kelimeler = new List<string>() { "fayik","Veznedaroglu","Fırat Üniversitesi","Yazılım Mühendisligi"};
+            //sayilar.Add(10);string ifade tanımlaması yaptığımzı için int türünden değişken tanımlaması yapamayız.
+            kelimeler.Add("Software");
+            kelimeler.Add(program);
+
+            kelimeler.AddRange(seri);//aşağıdaki foreach döngüsüyle aynı anlama gelmektedir.
+            kelimeler.AddRange(new string[] { "grape","moon","stars" });
+            //Insert işlemiyle'de ekleme işlemini yapmış oluruz.
+
+            //foreach (var s in seri)
+            //{
+            //    //kelimeler.Add(s);bu şekilde sonradan aynı tür oluşturulan farklı bir nesneyi diğer nesneyle birleştirme işlemine tabi tutabiliriz.
+            //}
+            //kelimeler.Remove("grape"); grape karakterini dizi'den silecektir.
+            //kelimeler.RemoveAt(1);  Yapılara göre 1.indeksteki yeri silecektir.
+
+            foreach (string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+
+            
+            
+            
+                       
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metod calisti");
+            int buyuk=Karsilastir(1,2);//herhangi bir nesne oluşturmadan bu metoda ulaşmak için metodu tanımlarken 'static' tanımlamamız gerekecektir.
+            Console.WriteLine(buyuk);
+            Console.WriteLine("Metod calismayi bitirdi.");
+
+           double sonuc = KareAl(4);
+            var sonuc2 = KareAl(3);
+            Console.WriteLine(sonuc2);
+            Console.WriteLine(sonuc);
+        }
+        public static int Karsilastir(int A,int B)
+        {
+            return A > B ? A : B;//yandaki kod parçacığı ile eğer koşul uygunsa A'yı,koşul uygun değilse B'yi dön diyoruz.
+        }
+        static double KareAl(double sayi)
+        {
+            /*double kare = sayi * sayi*/;
+            return sayi*sayi;
+        }
+    }
+}
